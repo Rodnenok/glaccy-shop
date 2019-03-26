e0977fb723dd2081cc632d40dae16dd961add674
Dmitry Rodnenok <dmitry.rodnenok@gmail.com>
2019-03-26 15:56:41 +0300

    slider+bg

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var modal = document.querySelector("#modal-mask"),
   modalOverlay = document.querySelector("#dark-background"),
   closeButton = document.querySelector("#close-button"),
-  openButton = document.querySelector("#open-button");
-
+  openButton = document.querySelector("#open-button"),
+  test = document.querySelector(".slider-list-item:nth-of-type(1)");
+console.log(test);
 function closeModal() {
   modal.style.display = "none";
   modalOverlay.style.display = "none";
